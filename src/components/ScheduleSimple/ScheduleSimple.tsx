307e0c5ef4a2bffd8602535e060c23ff25a57434
emb <ezequielmartinbenitez@gmail.com>
2023-03-08 10:44:35 -0300

    use sass, getTimes for schedule items, remove data knowledge from component

--- src/components/ScheduleSimple/ScheduleSimple.tsx
+++ src/components/ScheduleSimple/ScheduleSimple.tsx
@@ -1,49 +1,15 @@
-import { useRef } from "react";
-import { useInterval } from "../../hooks/useInterval";
-import type { ProgramWithTimes } from "../../hooks/useScheduleFetch";
-import "./css/ScheduleSimple.css";
+import { useState, useEffect } from 'react'
+import { orderBy } from 'lodash'
+import { useInterval } from '../../hooks/useInterval'
+import { getTimeSpan, TimeSpan } from '../../utils/getTimeSpan'
+import './ScheduleSimple.scss'
 
-const getTimeText = ({
-  hasStarted,
-  hasEnded,
-  startOf,
-  endOf,
-  startTime,
-  endTime,
-}: ProgramWithTimes["times"]) => {
-  let text = "";
-  if (!hasStarted && !hasEnded) {
-    text = `in ${startOf(startTime)}`;
-  } else if (hasStarted && !hasEnded) {
-    text = `${endOf(endTime)} left`;
-  }
-  return text;
-};
-
-const TimeText = ({
-  programTimes,
-}: {
-  programTimes: ProgramWithTimes["times"];
-}) => {
-  const timeText = useRef(getTimeText(programTimes));
-
-  useInterval(() => {
-    timeText.current = getTimeText(programTimes);
-  }, 20000);
-
-  return (
-    <div className="guide__item-time">
-      <span>{timeText.current}</span>
-    </div>
-  );
-};
-
-function NoPrograms(message: string = "No programs") {
+function NoPrograms(message: string = 'No programs') {
   return (
     <div id="schedule-simple" className="guide__list guide__list--EMPTY">
       <div className="guide__item">{message}</div>
     </div>
-  );
+  )
 }
 
 function Loading() {
@@ -51,65 +17,94 @@ function Loading() {
     <div id="schedule-simple" className="guide__list  guide__list--LOADING">
       <div className="guide__item">Loading...</div>
     </div>
-  );
+  )
 }
 
+type ScheduleGroup = {
+  id: string
+  start: number
+  end: number
+  title: string
+  description: string
+  // image?: string,
+}
 type ScheduleSimpleProps = {
-  scheduleGroup: ProgramWithTimes[];
-  loading?: boolean;
-  error?: boolean;
-};
+  scheduleGroup: ScheduleGroup[]
+  loading?: boolean
+  error?: boolean
+}
 
-function ScheduleSimple({
-  scheduleGroup,
-  loading,
-  error,
-}: ScheduleSimpleProps) {
+function ScheduleSimple({ scheduleGroup, loading, error }: ScheduleSimpleProps) {
+  const getTimes = () => {
+    if (!scheduleGroup) {
+      return []
+    }
+    const scheduleOrdered = orderBy(scheduleGroup, ['start'], ['asc'])
+    const times: TimeSpan[] = scheduleOrdered.map((item) =>
+      getTimeSpan(item.start, item.end, { timeOf: 'seconds' })
+    )
+    return times
+  }
+  const [scheduleTimes, setScheduleTimes] = useState<TimeSpan[]>(getTimes())
+
+  // Init Interval
+  useInterval(() => {
+    setScheduleTimes(getTimes())
+  }, 20000) // 20 seconds
+
+  useEffect(() => {
+    setScheduleTimes(getTimes())
+  }, [scheduleGroup])
+
   if (error) {
-    return NoPrograms();
+    return NoPrograms()
   }
   if (!scheduleGroup) {
     if (loading === true) {
-      return Loading();
+      return Loading()
     }
-    return NoPrograms();
+    return NoPrograms()
   }
 
   return (
     <div id="schedule-simple" className="guide__list">
-      {scheduleGroup.map((item: ProgramWithTimes) => {
-        const { times: programTimes } = item;
-        const startedStyle = programTimes.hasStarted
-          ? "guide__item--STARTED"
-          : "";
-        const endedStyle = programTimes.hasEnded ? "guide__item--ENDED" : "";
+      {scheduleTimes.length > 0 &&
+        scheduleGroup.map((item, index) => {
+          const times = scheduleTimes[index]
 
-        return (
-          <div
-            className={`guide__item ${startedStyle} ${endedStyle}`}
-            key={item.id}
-          >
-            <TimeText programTimes={programTimes} />
-            <div className="guide__item-body">
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
+          let timeText = ''
+          if (!times?.hasStarted() && !times?.hasEnded()) {
+            timeText = `in ${times?.startOf(times?.startTime)}`
+          } else if (times?.hasStarted() && !times?.hasEnded()) {
+            timeText = `${times?.endOf(times?.endTime)} left`
+          }
+
+          const startedStyle = times?.hasStarted() ? 'guide__item--STARTED' : ''
+          const endedStyle = times?.hasEnded() ? 'guide__item--ENDED' : ''
+
+          return (
+            <div className={`guide__item ${startedStyle} ${endedStyle}`} key={item.id}>
+              <div className="guide__item-time">
+                <span>{timeText}</span>
+              </div>
+              <div className="guide__item-body">
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </div>
+              <div className="guide__item-percentage">
+                <div
+                  className="guide__item-percentage-bar"
+                  data-value={times?.getPercentageCompleted()}
+                  style={{
+                    width: `${(times?.getPercentageCompleted() * 100).toFixed()}%`,
+                  }}
+                ></div>
+              </div>
             </div>
-            <div className="guide__item-percentage">
-              <div
-                className="guide__item-percentage-bar"
-                data-value={programTimes.getPercentageCompleted()}
-                style={{
-                  width: `${(
-                    programTimes.getPercentageCompleted() * 100
-                  ).toFixed()}%`,
-                }}
-              ></div>
-            </div>
-          </div>
-        );
-      })}
+          )
+        })}
     </div>
-  );
+  )
 }
 
-export { ScheduleSimple };
+export { ScheduleSimple }
